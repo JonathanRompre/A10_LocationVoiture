9f363ba9cf3a1938403c9d05019a8c8e4d9d301b
SamuelGrenier1 <Samuel.grenier89@gmail.com>
2022-11-25 10:06:06 -0500

    AjoutAuto
    compte auto 10

--- src/main/java/com/example/a10tpjonathan_rompresamuel_grenier/configuration/AutomobilesConfig.java
+++ src/main/java/com/example/a10tpjonathan_rompresamuel_grenier/configuration/AutomobilesConfig.java
@@ -19,9 +19,15 @@ public class AutomobilesConfig {
             Automobile automobile3 = new Automobile(2020, "Dodge", "Ram 1500", "4X4", "Manuelle", "F365H45", 698.45);
             Automobile automobile4 = new Automobile(2019, "Hyundai", "Kona", "Traction", "Automatique", "F345X67", 322.68);
             Automobile automobile5 = new Automobile(2022, "Toyota", "RAV4", "AWD", "Manuelle", "F867Y12", 597.65);
+            Automobile automobile6 = new Automobile(2022, "Toyota", "Camry", "Traction", "Automatique", "F654I41", 612.92);
+            Automobile automobile7 = new Automobile(2022, "Dodge", "Charger", "Traction", "Manuelle", "F0983K4", 545.17);
+            Automobile automobile8 = new Automobile(2022, "Mazda", "3-GT", "AWD", "Manuelle", "F934R00", 598.14);
+            Automobile automobile9 = new Automobile(2022, "Hyundai", "Tucson", "AWD", "Automatique", "F345C87", 597.65);
+            Automobile automobile10 = new Automobile(2022, "Dodge", "Durango", "4X4", "Automatique", "F093H76", 717.58);
 
 
-            automobilesRepository.saveAll(List.of(automobile1, automobile2, automobile3, automobile4, automobile5));
+
+            automobilesRepository.saveAll(List.of(automobile1, automobile2, automobile3, automobile4, automobile5, automobile6, automobile7, automobile8, automobile9, automobile10));
 
         };
     }
